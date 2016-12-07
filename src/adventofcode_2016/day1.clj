ba8414354ec2910f19c6cee2a44466f363ce3169
Eric Stewart <eric.stewart@impactivesystems.com>
2016-12-07 17:21:38 -0600

    Day1 part 2 solved and some minor refactoring done

--- src/adventofcode_2016/day1.clj
+++ src/adventofcode_2016/day1.clj
@@ -24,41 +24,63 @@
               4))))
 
 (defn new-coordinates
-  [current-position direction distance]
+  [[current-x current-y] direction distance]
   (let [modifier-tuple (case direction
                          :north [0 distance]
                          :east  [distance 0]
                          :south [0 (* -1  distance)]
-                         :west  [(* -1 distance) 0])]
-    [(+ (first current-position)
-        (first modifier-tuple))
-     (+ (last current-position)
-        (last modifier-tuple))]))
+                         :west  [(* -1 distance) 0]) 
+        new-x (+ current-x
+                 (first modifier-tuple))
+        new-y (+ current-y
+                 (last modifier-tuple))]
+    (do 
+      (if (#{:north :south} direction)
+        (map (fn [y]
+                [direction [new-x y]])
+              (range (+ (if (= direction :south) -1 1) current-y) 
+                     (if (= direction :south) (- new-y 1) (+ new-y 1))
+                     (if (= direction :south) -1 1)))
+        (map (fn [x]
+                [direction [x new-y]])
+              (range (+ (if (= direction :west) -1 1) current-x)
+                     (if (= direction :west) (- new-x 1) (+ new-x 1))
+                     (if (= direction :west) -1 1)))))))
 
 (defn move
   [current-position movement]
-  (let [direction (new-direction (first current-position)
-                                 (first (s/trim movement)))
-        distance (string->integer (apply str (rest (s/trim  movement))))]
-    [direction (new-coordinates (last current-position)
-                                direction
-                                distance)]))
+  (let [direction (new-direction (first (last current-position))
+                                 (first movement))
+        distance (string->integer (apply str (rest movement)))]
+    (new-coordinates (second (last current-position))
+                     direction
+                     distance)))
 
 (defn move-blocks
   [instructions]
   "From a starting location, move according to provide instructions
    and return the ending coordinates"
-  (let [movements (s/split instructions #",")]
-    (reduce move [:north [0,0]] movements)))
+  (let []
+    (->> (map str instructions)
+         (reductions move [[:north [0 0]]])
+         (apply concat)
+         (map second)
+         (reduce (fn [memory location]
+                   (if ((:visited memory) location)
+                     (reduced 
+                      (assoc memory :location location :visited (conj (:visited memory) location)))
+                     (assoc memory :location location :visited (conj (:visited memory) location))))
+                 {:location [] :visited  #{}}))))
 
-(defn distance-between
-  [start end]
-  (+ (math/abs (- (first end)
-                  (first start)))
-     (math/abs (- (last end)
-                  (last start)))))
+(defn taxicab-distance
+  [[end-x end-y]]
+  "Calculate the taxicab distance assuming an origin of [0 0]"
+  (+ (math/abs end-x)
+     (math/abs end-y)))
 
-(def problem-instructions "L1, L5, R1, R3, L4, L5, R5, R1, L2, L2, L3, R4, L2, R3, R1, L2, R5, R3, L4, R4, L3, R3, R3, L2, R1, L3, R2, L1, R4, L2, R4, L4, R5, L3, R1, R1, L1, L3, L2, R1, R3, R2, L1, R4, L4, R2, L189, L4, R5, R3, L1, R47, R4, R1, R3, L3, L3, L2, R70, L1, R4, R185, R5, L4, L5, R4, L1, L4, R5, L3, R2, R3, L5, L3, R5, L1, R5, L4, R1, R2, L2, L5, L2, R4, L3, R5, R1, L5, L4, L3, R4, L3, L4, L1, L5, L5, R5, L5, L2, L1, L2, L4, L1, L2, R3, R1, R1, L2, L5, R2, L3, L5, L4, L2, L1, L2, R3, L1, L4, R3, R3, L2, R5, L1, L3, L3, L3, L5, R5, R1, R2, L3, L2, R4, R1, R1, R3, R4, R3, L3, R3, L5, R2, L2, R4, R5, L4, L3, L1, L5, L1, R1, R2, L1, R3, R4, R5, R2, R3, L2, L1, L5")
+(def problem-instructions (read-string  "[L1, L5, R1, R3, L4, L5, R5, R1, L2, L2, L3, R4, L2, R3, R1, L2, R5, R3, L4, R4, L3, R3, R3, L2, R1, L3, R2, L1, R4, L2, R4, L4, R5, L3, R1, R1, L1, L3, L2, R1, R3, R2, L1, R4, L4, R2, L189, L4, R5, R3, L1, R47, R4, R1, R3, L3, L3, L2, R70, L1, R4, R185, R5, L4, L5, R4, L1, L4, R5, L3, R2, R3, L5, L3, R5, L1, R5, L4, R1, R2, L2, L5, L2, R4, L3, R5, R1, L5, L4, L3, R4, L3, L4, L1, L5, L5, R5, L5, L2, L1, L2, L4, L1, L2, R3, R1, R1, L2, L5, R2, L3, L5, L4, L2, L1, L2, R3, L1, L4, R3, R3, L2, R5, L1, L3, L3, L3, L5, R5, R1, R2, L3, L2, R4, R1, R1, R3, R4, R3, L3, R3, L5, R2, L2, R4, R5, L4, L3, L1, L5, L1, R1, R2, L1, R3, R4, R5, R2, R3, L2, L1, L5]"))
 
-(distance-between [0 0]
-                  (last (move-blocks problem-instructions)))
+#_(move-blocks problem-instructions)
+
+#_(distance-between [0 0]
+                    (:location (move-blocks problem-instructions)))
--- test/adventofcode_2016/day1_test.clj
+++ test/adventofcode_2016/day1_test.clj
@@ -26,44 +26,49 @@
 
 (deftest move-test
   (testing "Movement"
-    (is (= [:south [1 -1]]
-           (day1/move [:east [1, 0]] "R1")))
-    (is (= [:east [2 0]]
-           (day1/move [:north [0, 0]] "R2")))))
+    (is (= [[:south [1 -1]]]
+           (day1/move [[:east [1, 0]]] "R1")))
+    (is (= [[:east [1 0]][:east [2 0]]]
+           (day1/move [[:north [0, 0]]] "R2")))))
 
 (deftest new-coordinates-test
   (testing "New coordinates"
-    (is (= [1 0]
+    (is (= '([:east [1 0]])
            (day1/new-coordinates [0, 0] :east 1)))
-    (is (= [1 2]
+    (is (= '([:north [1 2]])
            (day1/new-coordinates [1 1] :north 1)))))
 
 (deftest one-block
   (testing "Move per instructions"
-    (is (= [:east [1 0]] 
-           (day1/move-blocks "R1")))
-    (is (= [:south [1 -1]] 
-           (day1/move-blocks "R1,R1")))
-    (is (= [:south [0 0]] 
-           (day1/move-blocks "R1, L1, L1, L1")))
-    (is (= [:east [125 0]] 
-           (day1/move-blocks "R125")))
-    (is (= [:north [2 3]]
-           (day1/move-blocks "R2, L3")))
-    (is (= [:west [0 -2]]
-           (day1/move-blocks "R2, R2, R2")))))
+    (is (= [1 0] 
+           (:location (day1/move-blocks ["R1"]))))
+    (is (= [1 -1] 
+           (:location (day1/move-blocks ["R1","R1"]))))
+    (is (= [0 0] 
+           (:location (day1/move-blocks ["R1" "L1" "L1" "L1"]))))
+    (is (= [125 0] 
+           (:location (day1/move-blocks ["R125"]))))
+    (is (= [2 3]
+           (:location (day1/move-blocks ["R2" "L3"]))))
+    (is (= [0 -2]
+           (:location (day1/move-blocks ["R2" "R2" "R2"]))))))
+
+(deftest shortest-path-to-destination
+  (testing "Stop at first coordinates encountered twice"
+    (is (= [4, 0]
+           (:location (day1/move-blocks ["R8" "R4" "R4" "R8"]))))))
 
 (deftest calc-distance
   (testing "Calculate taxicab distance"
     (is (= 1
-           (day1/distance-between [0 0] [1 0])))
+           (day1/taxicab-distance [1 0])))
     (is (= 1
-           (day1/distance-between [0 0] [-1 0]))) 
+           (day1/taxicab-distance [-1 0]))) 
     (is (= 2
-           (day1/distance-between [0 0] [1 1]))) 
+           (day1/taxicab-distance [1 1]))) 
     (is (= 4
-           (day1/distance-between [0 0] [2 2])))
+           (day1/taxicab-distance [2 2])))
     (is (= 4
-           (day1/distance-between [0 0] [-2 -2])))
+           (day1/taxicab-distance [-2 -2])))
     (is (= 125 
-           (day1/distance-between [0 0] [125 0])))))
+           (day1/taxicab-distance [125 0])))))
